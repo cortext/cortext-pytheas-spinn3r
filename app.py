e26173c763a3fe2102ab1ef785cc85e91226f2e4
ikario404 <ikar77@hotmail.com>
2018-02-16 13:38:47 +0100

    add error template and Oauth method

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-import os
-import io
 import json
+import os
 import csv
+import io
 import requests
 from get_data import loop_download
-
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
 from flask import send_file
 from flask import Response
 from flask import jsonify
+from flask import redirect
+from flask import url_for
 from flask_bootstrap import Bootstrap
+from uuid import uuid4
 
+## packagin app
 def create_app():
 	app = Flask(__name__)
 	Bootstrap(app)
@@ -27,8 +31,27 @@ try:
 except BaseException as error:
 	print('An exception occurred : {}'.format(error))
 
+## before rendering app
+@app.before_request
+def before_request():
+    try:
+        # session['api_key'] = app.config['api_key']
+        if 'access_token' not in session and request.endpoint != 'login':
+            if 'auth' in request.endpoint:
+                return auth()
+            elif 'grant' in request.endpoint:
+                return grant()            
+            return redirect(url_for('login'))
+    except BaseException as e:
+        print(e)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', error=error)
+
+## start rendering app.html
 @app.route("/")
-def hello():
+def home():
 	print('hello')
 	return render_template('home.html')
 
@@ -40,7 +63,7 @@ def processing():
 	# print(request.form)
 	simple_q = str(request.form.get('query-simple'))
 	advanced_q = str(request.form.get('query-advanced'))
-	pprint(advanced_q)
+	#pprint(advanced_q)
 
 	# filters
 	# infinite = request.form.get('infinite')
@@ -51,9 +74,8 @@ def processing():
 	q = json.loads(advanced_q)
 	query = str(q['query']['query_string']).replace(" ","_")
 
-	
-	pprint(advanced_q)
-	pprint(q)
+	#pprint(advanced_q)
+	#pprint(q)
 
 	# download query
 	bucket_data = loop_download(advanced_q)
@@ -100,6 +122,62 @@ def processing():
 	else:
 		return render_template('home.html')
 
+##########################################################################
+# OAuth
+##########################################################################
+@app.route('/login')
+def login():   
+    return render_template('login.html')
+
+@app.route('/grant', methods=['GET'])
+def grant():
+    with open('conf/conf.json') as conf_file:
+        conf_data = json.load(conf_file)
+        grant_host_url = conf_data['grant_host_url']
+        redirect_uri_conf = conf_data['redirect_uri']
+
+    grant_url = grant_host_url + "/auth/authorize" + \
+                "?response_type=code" + \
+                "&state=" + str(uuid4().hex) + \
+                "&client_id=pytheas" + \
+                "&redirect_uri=" + redirect_uri_conf
+
+    headers = {
+        'Location': grant_url
+    }
+
+    return Response(grant_url, status=302, headers=headers)
+
+@app.route('/auth', methods=['GET'])
+def auth():
+    code = str(request.args['code']) 
+    state = str(request.args['state']) 
+
+    with open('conf/conf.json') as conf_file:
+        conf_data = json.load(conf_file)
+        redirect_uri_conf = conf_data['redirect_uri']
+        grant_host_url = conf_data['grant_host_url']
+
+    payload = {
+      'code': code,
+      'state': state,
+      'client_id': 'pytheas',
+      'client_secret': 'mys3cr3t',
+      'redirect_uri': redirect_uri_conf,
+      'grant_type': 'authorization_code'
+    }
+
+    r_grant = requests.post(grant_host_url + '/auth/grant', data=payload)
+    data = r_grant.json()
+    print()
+    print(data)
+    print()
+    r_access = requests.get(grant_host_url + '/auth/access?access_token=' + str(data['access_token']))
+    
+    session['access_token'] = data['access_token']
+    session['profil'] = r_access.json()
+
+    return redirect(url_for('home'))
 
 ##########################################################################
 # Start
